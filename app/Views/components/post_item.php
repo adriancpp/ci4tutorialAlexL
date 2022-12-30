36c1e861d78bf4bf998f3b5f56e93090ee99f4f0
adriancpp <adrian.cpp@wp.pl>
2022-12-30 21:20:35 +0100

    cells

---
+++ app/Views/components/post_item.php
@@ -0,0 +1,9 @@
+
+<div class="card col-12 col-sm-6 col-md-4" style="width: 18rem;">
+    <img class="card-img-top" src="/assets/images/avatar.png" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title"><?= $title ?></h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus at augue eget arcu dictum varius duis. Magna etiam tempor orci eu lobortis elementum. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Fames ac turpis egestas sed tempus urna et pharetra. Proin nibh nisl condimentum id venenatis a condimentum vitae. Nunc aliquet bibendum enim facilisis gravida neque convallis a. Phasellus vestibulum lorem sed risus ultricies. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque. A erat nam at lectus. Massa vitae tortor condimentum lacinia quis vel eros donec ac. Vel risus commodo viverra maecenas. Nec feugiat nisl pretium fusce id velit ut tortor pretium. Amet purus gravida quis blandit turpis cursus in hac habitasse.</p>
+        <a href="/blog/post" class="btn btn-primary">Read more</a>
+    </div>
+</div>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title><?= (isset($meta_title)? $meta_title : 'Codeigniter default title') ?></title>
     <meta name="description" content="A simple HTML5 Template for new projects.">
@@ -24,9 +25,25 @@
 </head>
 
 <body>
-<nav>
-    <ul>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/blog/post">Post</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+
+    <a class="navbar-brand" href="/">CI4 Tut</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/blog">Blog </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/blog/post">Post</a>
+            </li>
+        </ul>
+        </div>
+    </div>
 </nav>
+<div class="container">
