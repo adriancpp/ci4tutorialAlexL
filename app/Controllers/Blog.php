d8a5a70e135cb01e0eaf89c3d0ebe57ab61831a3
adriancpp <adrian.cpp@wp.pl>
2023-01-07 14:50:54 +0100

    connection interface

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -3,11 +3,20 @@
 namespace App\Controllers;
 
 use App\Models\BlogModel;
+use App\Models\CustomModel;
 
 class Blog extends BaseController
 {
     public function index()
     {
+        $db = db_connect();
+
+        $model = new CustomModel($db);
+        echo '<pre>';
+            print_r($model->getPosts());
+            echo '<pre>';
+
+
         $data = [
             'meta_title' => 'Codeigniter 4 Blog',
             'title' => 'This is a Blog Page',
